60b1521b49ff240ede111e54d887c92838134464
Gerwin012 <gerwindeheus@icloud.com>
2025-08-01 23:04:04 +0200

    menu in de edit page gemaakt

--- dashboard/pages/edit.php
+++ dashboard/pages/edit.php
@@ -23,6 +23,58 @@ include('../includes/header.php');
     </div>
 </section>
 
+<section id="pages_edit">
+    <div class="container">
+        <div class="flex">
+            <div class="menu">
+                <ul>
+                <li><button class="active" data-section="page">Pagina</button></li>
+                <li><button data-section="settings">Widgets</button></li>
+                <li><button data-section="profiel">Instellingen</button></li>
+                </ul>
+            </div>
+            <div class="content">
+                <div class="content-section active" id="page">Content voor pagina</div>
+                <div class="content-section hidden" id="settings">Content voor Widgets</div>
+                <div class="content-section hidden" id="profiel">Content voor Instellingen</div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<script>
+  document.querySelectorAll('.menu button').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const sectionId = btn.getAttribute('data-section');
+
+    // Verberg alle secties
+    document.querySelectorAll('.content-section').forEach(section => {
+      section.classList.add('hidden');
+      section.classList.remove('active');
+    });
+
+    // Laat de gekozen sectie zien
+    const target = document.getElementById(sectionId);
+    target.classList.remove('hidden');
+    target.classList.add('active');
+
+    document.querySelectorAll('.menu button').forEach(b => b.classList.remove('active'));
+
+    // Active knop + sectie
+    btn.classList.add('active');
+    document.getElementById(sectionId).classList.remove('hidden');
+  });
+});
+
+// Standaard openen van 'pages'
+const defaultSection = document.getElementById('pages');
+defaultSection.classList.remove('hidden');
+defaultSection.classList.add('active');
+
+</script>
+
+
 
 <h3></h3>
 <?php include '../includes/footer.php'; ?>
